672e1718c47f491dfdcbde839fab77df98a4e84f
JZue <646980564@qq.com>
2021-05-11 15:36:33 +0800

    IntegerAtomicDemo

--- multi-thread/src/main/java/demo/thread/multithread/juc/atomic/IntegerAtomicDemo.java
+++ multi-thread/src/main/java/demo/thread/multithread/juc/atomic/IntegerAtomicDemo.java
@@ -1,9 +1,12 @@
 package demo.thread.multithread.juc.atomic;
 
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
 /**
  * AtomicInteger
  * AtomicIntegerArray
- * AtomicIntegerFieldUpdater
+ * AtomicIntegerFieldUpdater 基于反射的实用工具，可以对指定类的指定 volatile int 字段进行原子更新
+ *
  * @author jzue
  * @date 2021/5/11 3:13 下午
  **/
@@ -11,6 +14,33 @@ public class IntegerAtomicDemo {
 
     public static void main(String[] args) {
 
+    }
+
+    public static void demoAtomicInteger(){
+
+    }
+
+    public static void demoAtomicIntegerArray(){
+
+    }
+
+    public static void demoAtomicIntegerFieldUpdater(){
+        AtomicIntegerFieldUpdater<DemoEntity> demoEntityFliedAtomic = AtomicIntegerFieldUpdater.newUpdater(DemoEntity.class,"id");
+        DemoEntity demoEntity = new DemoEntity();
+
+        demoEntityFliedAtomic.addAndGet(demoEntity,1);
+        // 其他方法，用的时候一一看
+    }
+
+    static class DemoEntity{
+        private volatile  int id;
+        private String key;
+        public void setId(int id) {
+            this.id= id;
+        }
+        public int getId() {
+            return this.id;
+        }
     }
 
 
---
+++ multi-thread/src/main/java/demo/thread/multithread/juc/atomic/LongAtomicDemo.java
@@ -0,0 +1,8 @@
+package demo.thread.multithread.juc.atomic;
+
+/**
+ * @author jzue
+ * @date 2021/5/11 3:22 下午
+ **/
+public class LongAtomicDemo {
+}
